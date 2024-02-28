fff7514bb8421b2c035c32f25fa7555789dde3b9
shallowdream <870706390@qq.com>
2024-02-28 14:21:12 +0800

    完成微信用户登陆业务开发

---
+++ sky-server/src/main/java/com/sky/controller/user/UserController.java
@@ -0,0 +1,55 @@
+package com.sky.controller.user;
+
+import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.UserLoginDTO;
+import com.sky.entity.User;
+import com.sky.properties.JwtProperties;
+import com.sky.result.Result;
+import com.sky.service.UserService;
+import com.sky.utils.JwtUtil;
+import com.sky.vo.UserLoginVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@Slf4j
+@Api(tags = "C端用户相关接口")
+@RequestMapping("/user/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    @PostMapping("/login")
+    @ApiOperation("微信登陆")
+    public Result login(@RequestBody UserLoginDTO userLoginDTO){
+        log.info("微信用户登陆:{}",userLoginDTO.getCode());
+        //微信登陆
+        User user = userService.wxLogin(userLoginDTO);
+
+        Map<String,Object> claims = new HashMap<>();
+        claims.put(JwtClaimsConstant.USER_ID,user.getId());
+        String token = JwtUtil.createJWT(
+                jwtProperties.getUserSecretKey(),
+                jwtProperties.getUserTtl(),
+                claims);
+
+        UserLoginVO userLoginVO = UserLoginVO.builder()
+                .id(user.getId())
+                .openid(user.getOpenid())
+                .token(token)
+                .build();
+        log.info("debug{}",userLoginVO);
+        return Result.success(userLoginVO);
+    }
+
+}
